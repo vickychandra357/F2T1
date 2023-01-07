bb0a3744906c8737dabc852731900603ccbebaee
tonystark <chandravicky288@gmail.com>
2023-01-07 22:35:28 +0530

    Sol

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  let search1=arr.map((item )=>{
+    if(item.profession==='developer'){
+      console.log(item.name);
+    }
+  });
+  
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
-}
+  let search2=arr.forEach(item=>{
+    if(item.profession==='developer')
+    console.log(item.name);
+  });
+  
+
+} 
 
 function addData() {
   //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"});
+  console.log(arr);
+
 }
 
 function removeAdmin() {
   //Write your code here
+ function removeValue(profession){
+  if(profession==='admin'){
+  arr.splice()
+  return true;
+ }
+ return false;
 }
-
+let x=arr.filter(removeValue);
+console.log(x);
+}
 function concatenateArray() {
   //Write your code here
+let arr2=[
+  {id:5,name:"kumar",age:"22",profession:"business man"}
+]
+let arr3=arr.concat(arr2);
+console.log(arr3);
 }
